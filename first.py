c90ce9f96057810adca64dc96d708324f29328c6
HA QUANG MINH <quangminhh2004@gmail.com>
2024-07-08 17:37:41 +0700

    add first.py

---
+++ first.py
@@ -0,0 +1,34 @@
+import math
+from math import *
+a=123.213
+#print('%2f' %a)
+#print(round(a,2))
+#print(math.sqrt(36))
+#print(math.isqrt(46))
+#pow
+#ceil  lam tron len
+#floor lam tron duoi
+#factorial giai thua
+#gcd uoc chung lon nhat
+#comb (n,k) to hop
+#prerm(n,k) chinh hop
+#fabs() tri tuyrt doi
+#round lam tron
+#max() ,min()
+x = int(input())
+y = int(input())
+z = int(input())
+for  i in range(x) :
+    for j in range(y):
+        print(i,j) 
+
+#list :
+    #append them vao cuoi
+    #insert(x,y) x:vi tri ,y :so them vao
+    #pop(x) xoa phan tu x, ko thi xoa so cuoi
+    #a.extend(b) them b vao a hoac a+=b
+    #c=a.copy() c giong a nma ko phai la a
+    #a.count(x) so lan x xhien trong a
+    #a.index(x) vi tri cua x trong a
+    #a.reverse () lat nguoc 1 list 
+    #a.sort() sap xep trong a
